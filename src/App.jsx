d22a1382a4bbd52c1073f41b19840f28c7010f9d
Dipok Kumar Ray <dipok7316@gmail.com>
2025-04-09 02:21:01 +0600

    added Navbar

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Navbar  from './Navbar'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <h1>Vite + React</h1>
+      <Navbar></Navbar>
+    
+     
+    </>
+  )
+}
+
+export default App
