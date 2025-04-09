6bd0e54540dcda2b9e9442a28d74ef8ce9f7e8f4
Dipok Kumar Ray <dipok7316@gmail.com>
2025-04-09 17:10:09 +0600

    BidItems Table Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,56 @@
+import "./App.css";
+import Navbar from "./Navbar";
+import Banner from "./Banner";
+import BidItems from './BidItems';
+import Footer from "./Footer";
 
-import './App.css'
-import Navbar  from './Navbar'
-import Banner from './Banner'
-
-
+// import { useEffect, useState } from 'react';
 
 function App() {
+    // const [bids, setBids] = useState([]);
+
+    // useEffect(() => {
+    //     fetch('public/data.json')
+    //         .then(res => res.json())
+    //         .then(data => setBids(data));
+    // }, []);
+
+    return (
+        <>
+            <Navbar></Navbar>
+            <Banner></Banner>
+            <Footer></Footer>
+
+            <BidItems></BidItems>
+            {/* <div className="grid grid-cols-1  justify-around"> */}
+                {/* {bids.map((bid) => (
+                    <BidItems key={bid.id} bid={bid}></BidItems>
+                ))} */}
+              {/* <table>
+      <thead>
+        <tr>
+          <th>Item</th>
+          <th>Current Bid</th>
+        </tr>
+      </thead>
+      <tbody>
+      {
+        bids.map(bid => (
+          <tr key={bid.id}>
+            <td>{bid.title}</td>
+            <td>{bid.currentBidPrice}
+
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table> */}
 
 
-  return (
-    <>
-     
-      <Navbar></Navbar>
-      <Banner></Banner>
-     
-    </>
-  )
+
+            {/* </div> */}
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/BidItems.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from 'react';
+import { SlHeart } from "react-icons/sl";
+// const [bids, setBids] = useState([]);
+
+const BidItems = () => {
+    const [bids, setBids] = useState([]);
+
+    useEffect(() => {
+        fetch('public/data.json')
+            .then(res => res.json())
+            .then(data => setBids(data));
+    }, []);
+
+  return (
+    <div>
+      <div className="  text-start mt-10">
+        <h1 className="text-3xl font-bold">Active Auctions </h1>
+        <p className="text-xl font-semibold ">
+          Discover and bid on extraordinary items
+        </p>
+      </div>
+      <div className="flex">
+        <div className=" w-[70%] bg-slate-200 rounded-lg shadow-lg p-4 m-4">
+       
+        <table>
+      <thead >
+        <tr>
+          
+          <th >Item</th>
+          <th >Title</th>
+          <th >Current Bid</th>
+          <th >Time Left </th>
+          <th >Bid Now</th>
+          
+        </tr>
+      </thead >
+      <tbody >
+      {
+        bids.map(bid => (
+          <tr className='border-2 border-indigo-500 ' key={bid.id}>
+            <td ><img className='w-20' src={bid.image} alt="" srcset="" /></td>
+            <td>{bid.title}</td>
+            <td> $ {bid.currentBidPrice}
+            <td>{bid.timeLeft}</td>
+            <td> <SlHeart /></td>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+
+
+
+        </div>
+
+        <div className="right-container w-[30%] bg-slate-200 rounded-lg shadow-lg p-5 m-5 flex-row">
+         <div className='flex justify-center place-items-center gap-2  '>
+         < SlHeart />
+         <h1 className='text-blue-500 font-bold'>Favorite Items</h1>
+         </div>
+         <h1>Total Bids Amount : </h1>
+        </div>
+      </div>
+     </div>
+  );
+};
+
+export default BidItems;
